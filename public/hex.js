49efeebf4b060393e86665bb6f0d334ed82ddb36
Charlie <charjhall777@gmail.com>
2021-05-22 20:58:22 -0400

    new hexagons looking good!

--- public/hex.js
+++ public/hex.js
@@ -1,66 +1,69 @@
-let hexes = [];
-let x, y;
+const HEX_SIZE = 40;
 
-let past_x, buffer;
+let hexes = [];
+
+let colors = [
+	[13, 115, 119],
+	[20, 255, 236],
+	[158, 52, 0],
+	[235, 88, 16]
+];
 
 function setup() {
-	createCanvas($(window).width(), $(window).height());
-	let random_subtract = floor(random(15, 200));
+	createCanvas($(window).width(), $(window).height() - 42);
 
-	for (let hex_y = 0; hex_y < $(window).height() / 20; hex_y++) {
-		hexes[hex_y] = [];
-		past_x = (hex_y % 2) == 0 ? -30 : -60;
+	let random_subtract = floor(random(-30, -10));
+	let build_x, build_y
 
-		let rolling_x = 0;
-		let adder = hex_y % 2 == 1 ? 15 : 0;
-		for (let hex_x = 0; hex_x < $(window).width(); hex_x++) {
-			hexes[hex_y][hex_x] = new Hexes((rolling_x + adder) - random_subtract, hex_y * 20);
-			buffer = rolling_x;
-			rolling_x += buffer - past_x == 60 ? 30 : 60;
-			past_x += buffer - past_x == 60 ? 60 : 30;
+	for (let x = 0; x < $(window).width()/HEX_SIZE + 50; x++) {
+		hexes[x] = [];
+		for (let y = 0; y < $(window).height()/HEX_SIZE + 50; y++) {
+			build_x = y % 2 == 0 ? x * HEX_SIZE + random_subtract : x * HEX_SIZE - (HEX_SIZE / 2) + random_subtract;
+			build_y = y * HEX_SIZE + random_subtract;
+			hexes[x][y] = new OBJHexes(build_x, build_y);
 		}
 	}
 
-	strokeWeight(3);
+	background(0);
+	fill(0);
+	stroke(255);
 }
 
 function draw() {
-	for (let hex_y = 0; hex_y < $(window).height() / 20; hex_y++) {
-
-		for (let hex_x = 0; hex_x < $(window).width(); hex_x++) {
-			hexes[hex_y][hex_x].display();
-
-			if (abs(hexes[hex_y][hex_x].x - x) < 50 && abs(hexes[hex_y][hex_x].y - y) < 50)
-				stroke(20, 255, 236);
-			if (hex_y % 2 == 1) {
-				if (hex_x % 2 == 1) {
-					if (hexes[hex_y][hex_x - 1]) line(hexes[hex_y][hex_x].x, hexes[hex_y][hex_x].y, hexes[hex_y][hex_x - 1].x, hexes[hex_y][hex_x - 1].y);
-				}
-				if (hexes[hex_y - 1] && hexes[hex_y - 1][hex_x]) line(hexes[hex_y][hex_x].x, hexes[hex_y][hex_x].y, hexes[hex_y - 1][hex_x].x, hexes[hex_y - 1][hex_x].y);
-				if (hexes[hex_y + 1] && hexes[hex_y + 1][hex_x]) line(hexes[hex_y][hex_x].x, hexes[hex_y][hex_x].y, hexes[hex_y + 1][hex_x].x, hexes[hex_y + 1][hex_x].y);
-			} else {
-				if (hex_x % 2 == 1) {
-					if (hexes[hex_y][hex_x + 1]) line(hexes[hex_y][hex_x].x, hexes[hex_y][hex_x].y, hexes[hex_y][hex_x + 1].x, hexes[hex_y][hex_x + 1].y);
-					if (hexes[hex_y - 1] && hexes[hex_y - 1][hex_x]) line(hexes[hex_y][hex_x].x, hexes[hex_y][hex_x].y, hexes[hex_y - 1][hex_x].x, hexes[hex_y - 1][hex_x].y);
-					if (hexes[hex_y + 1] && hexes[hex_y + 1][hex_x]) line(hexes[hex_y][hex_x].x, hexes[hex_y][hex_x].y, hexes[hex_y + 1][hex_x].x, hexes[hex_y + 1][hex_x].y);
-				}
-			}
+	for (let x = 0; x < hexes.length; x++) {
+		for (let y = 0; y < hexes[x].length; y++) {
+			hexes[x][y].display();
 		}
 	}
 }
 
 $(document).mousemove((event) => {
-	x = event.pageX;
-	y = event.pageY;
+	hexes[floor(event.pageX / HEX_SIZE)][floor(event.pageY / HEX_SIZE)].color = colors[3];
+	hexes[floor(event.pageX / HEX_SIZE)][floor(event.pageY / HEX_SIZE)].change = 80;
 });
 
-function Hexes(hex_x, hex_y) {
+function OBJHexes(hex_x, hex_y) {
 	this.x = hex_x;
 	this.y = hex_y;
-	this.color = color(235, 88, 16);
+	this.color = colors[0];
+	this.change = 0;
 
-	this.display = function() {
+	this.display = function(){
+		if (this.change != 0) {
+			this.change--;
+			this.color = color(red(this.color) - 5, green(this.color) - 5, blue(this.color) - 5);
+			if (this.change == 0) this.color = 0;
+		}
 		stroke(this.color);
-		point(this.x, this.y);
+
+		let y_subtract = HEX_SIZE / 2 - 5;/*this.y / HEX_SIZE % 2 == 1 ? HEX_SIZE / 2 - 15 : HEX_SIZE / 2;*/
+		beginShape();
+		vertex(this.x, this.y - 25);
+		vertex(this.x + (HEX_SIZE / 2), this.y - y_subtract);
+		vertex(this.x + (HEX_SIZE / 2), this.y + y_subtract);
+		vertex(this.x, this.y + 25);
+		vertex(this.x - (HEX_SIZE / 2), this.y + y_subtract);
+		vertex(this.x - (HEX_SIZE / 2), this.y - y_subtract);
+		endShape(CLOSE);
 	}
 }
