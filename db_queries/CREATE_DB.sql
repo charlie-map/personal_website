806fa78a7dddb74fc3b040b0ec0a614b918122f3
Charlie <charjhall777@gmail.com>
2021-05-28 18:48:52 -0400

    setting up db

---
+++ db_queries/CREATE_DB.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE my_site;
+USE my_site;
+
+CREATE USER 'your username'@'your host name' IDENTIFIED BY 'your password here';
--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+require('dotenv').config({
+	path: __dirname + "/.env"
+});
 const express = require('express');
 const bodyParser = require('body-parser');
 const mustache = require('mustache-express');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+	host: process.env.HOST,
+	database: process.env.DATABASE,
+	user: process.env.USER_NAME,
+	password: process.env.PASSWORD,
+	insecureAuth: false
+});
+
+connection.connect((err) => {
+	if (err) throw err;
+});
 
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
-    extended: false
+	extended: false
 }));
 app.set('views', __dirname + "/views");
 app.set('view engine', 'mustache');
@@ -17,7 +33,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/go-to-page/:page", (req, res) => {
-	
+
 });
 
 app.listen(9988, () => {
